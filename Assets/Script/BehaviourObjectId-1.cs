7502bed28e2a30a4db13f23ec666b6d76c3307e8
Petroscheva <pjhvandegeer@gmail.com>
2026-06-18 11:53:40 +0200

    Initial commit

---
+++ Assets/Script/BehaviourObjectId-1.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class BehaviourObjectId : MonoBehaviour
+{
+    [Tooltip("Must match the '#' value in the CSV (e.g. '1', '2', '3', ...)")]
+    public string objectId;
+}
