f15e2b16d4f116a39ecc300fe78a6d6c51b5fea9
Petroscheva <pjhvandegeer@gmail.com>
2026-06-25 09:54:20 +0200

    hopelijk is dit goed

--- bezoekerssimulatie-Wereldmuseum-x-Responsible-IT/bezoekerssimulatie-Wereldmuseum-x-Responsible-IT/Assets/Script/BehaviourObjectId-1.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class BehaviourObjectId : MonoBehaviour
-{
-    [Tooltip("Must match the '#' value in the CSV (e.g. '1', '2', '3', ...)")]
-    public string objectId;
-}
